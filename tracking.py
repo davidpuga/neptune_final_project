8ff30129521d5c43f0211ade99f0058926dc63bd
David Puga <david.puga@embl.de>
2016-06-09 18:56:47 +0200

    problem: tried to track several postits. line 70

--- tracking.py
+++ tracking.py
@@ -33,8 +33,6 @@ args = vars(ap.parse_args())
 
 orangeLower = (0, 124, 113) # will define the lower boundary in HSV color space of the orange post-it.
 orangeUpper = (15, 255, 255)
-pts = deque(maxlen=args["buffer"]) # pts will be a deque structure whose maximum length will be defined by the second argument of the script (i.e. the buffer)
- 
 
 
 # if a video path was not supplied, grab the reference
@@ -47,20 +45,12 @@ else:
 
 
 
-
-
-
-
-
-
 # keep looping
 while True:
-	# grab the current frame
-	(grabbed, frame) = camera.read()
+	
+	(grabbed, frame) = camera.read() # grab the current frame
  
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
-	if args.get("video") and not grabbed:
+	if args.get("video") and not grabbed: # if we are viewing a video and we did not grab a frame, then we have reached the end of the video
 		break
  
 
@@ -72,44 +62,45 @@ while True:
 	mask = cv2.inRange(hsv, orangeLower, orangeUpper) # it will create a mask for every object in that falls into the HSV range defined
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2) #dilate and erode to get rid of small defects in the mask
-
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] # cnts is the ensemble of countours that are in the mask (the objects with defined contours that are orange). RETR_EXTERNAL will outline only the external contour. CHAIN_APPROX_SIMPLE will draw the contour with the least number of points.
-	center = None
- 
+ 	center = None
 	
 	if len(cnts) > 0: # len(list) is the number of items in list. It will proceed only if at least one (orange object) was found.
 
-		c = max(cnts, key=cv2.contourArea) # max will look in cnts and find the largest object as defined by their contour area
-		(x, y, w, h) = cv2.boundingRect(c) # of the largest object, it computes the 
-		M = cv2.moments(c) 
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		for postits in cnts: #added this so that it will track several post-its at once
+			
+			pts = deque(maxlen=args["buffer"]) # pts will be a deque structure whose maximum length will be defined by the second argument of the script (i.e. the buffer)
+		
+		#	c = max(cnts, key=cv2.contourArea) # max will look in cnts and find the largest object as defined by their contour area. Had to take this out to track several at once.
+		
+			(x, y, w, h) = cv2.boundingRect(postits) # defines variables of postits as being a rectangle
+			M = cv2.moments(postits) 
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
  
-		# only proceed if the width meets a minimum size
-		if w > 10:
-			# draw the rectangle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.rectangle(frame, (int(x), int(y)), (x+w,y+h), (0, 255, 0), 3) # 1st arg: image, 2nd: upper-left corner, 3rd: lower-right, 4th: color, 5th: thickness
-			cv2.rectangle(frame, center, center, (0, 255, 0), -1)
+			# only proceed if the width meets a minimum size
+			if w > 10:
+				# draw the rectangle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.rectangle(frame, (int(x), int(y)), (x+w,y+h), (0, 255, 0), 3) # 1st arg: image, 2nd: upper-left corner, 3rd: lower-right, 4th: color, 5th: thickness
+				cv2.rectangle(frame, center, center, (0, 255, 0), -1)
  
-	pts.appendleft(center) # as center (centroid) moves, it will create a list by adding each new point. By default, the buffer is 32, so there will be 32 points in total. If I append to the right, the centroid will always have 1 as I move along and 31 will be added and then that point will dissapear. If I append to the left, the centroid will always have 32 points which will progressively dissapear in the tail. So for visualizing, it's better to append to the left, so we have a big point at the centroid, and the tail becomes thinner.
+			pts.appendleft(center) # as center (centroid) moves, it will create a list by adding each new point. By default, the buffer is 32, so there will be 32 points in total. If I append to the right, the centroid will always have 1 as I move along and 31 will be added and then that point will dissapear. If I append to the left, the centroid will always have 32 points which will progressively dissapear in the tail. So for visualizing, it's better to append to the left, so we have a big point at the centroid, and the tail becomes thinner.
 
-		# loop over the set of tracked points
-	for i in xrange(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
- 
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		color = np.random.randint(0,255,3) # creates random integers. 1st: lowest number, 2nd: highest, 3rd: size of the array
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], color, thickness)
+				
+			for i in xrange(1, len(pts)): # loop over the set of tracked points
+	
+				if pts[i - 1] is None or pts[i] is None: # if either of the tracked points are None, ignore them
+					continue
+ 	
+
+				color = np.random.randint(0,255,3) # creates random integers. 1st: lowest number, 2nd: highest, 3rd: size of the array
+				thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+				cv2.line(frame, pts[i - 1], pts[i], color, thickness)
  
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
-	cv2.imshow("Mask", mask)
-	cv2.moveWindow("Mask",700,110)
+#	cv2.imshow("Mask", mask)
+#	cv2.moveWindow("Mask",700,110)
 
 	key = cv2.waitKey(1) & 0xFF
  
@@ -117,10 +108,6 @@ while True:
 	if key == ord("q"):
 		break
  
-
-
-
-
 
 # cleanup the camera and close any open windows
 camera.release()
