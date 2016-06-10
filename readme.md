07385481daceb2a01b6358dd0935cc2b0072b78e
David Puga <david.puga@embl.de>
2016-06-10 09:56:08 +0200

    updated discussion

--- readme.md
+++ readme.md
@@ -9,10 +9,8 @@ The goal of my project is to automatically identify the post-its on my webcam an
 
 The methods I will use to do this are:
 * The [program](https://github.com/davidpuga/neptune_final_project/blob/master/tracking.py) I'm using is based on [this](http://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/) and [this](http://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/) program.
-* I will use the following python packages: deque, numpy, argparse, imutils, cv2
+* I will use the following python packages: deque, numpy, argparse, imutils, cv2, SciPy
 * The scripts [range-detector](https://github.com/jrosebr1/imutils/blob/master/bin/range-detector) to obtain the HSV ranges for the orange post-it.
-
-I will use my webcam.
 
 
 ## Methods
@@ -27,11 +25,13 @@ In Figure 1...
 
 ## Discussion
 
-These results indicate...
+Identifying an object on a video can be done according to different variables of the object, such as color, shape or movement. In this program, I used the color of the post-its to differentiate them from the background and identify them.
+In order to track an object, the object needs to be recognized as an entity throughout the video, i.e. from one frame to the next. My first script, which recognized and tracked a single post-it, identified the largest orange post-it and followed it. This truly tracks the post-it if only a single post-it is in the webcam's field of view, or if it is always the largest. But if more than one post-it is present and their relative sizes keep changing (because they get closer or further away from the webcam), the tracking will follow the largest post-it at any given frame, even if it is not the same one.
+A solution to this, which also opens the possibility to track several post-its simultaneously, is to track 
 
 The biggest difficulty in implementing these analyses was...
 
-If I did these analyses again, I would...
+My
 
 ## References
 
--- tracking.py
+++ tracking.py
@@ -9,6 +9,7 @@ import numpy as np
 import argparse # is a module to help manage the arguments for the command-line
 import imutils
 import cv2
+from scipy.spatial import distance as dist
 ###############################################
 
 
@@ -33,6 +34,7 @@ args = vars(ap.parse_args())
 
 orangeLower = (0, 124, 113) # will define the lower boundary in HSV color space of the orange post-it.
 orangeUpper = (15, 255, 255)
+pts = deque(maxlen=args["buffer"]) # pts will be a deque structure whose maximum length will be defined by the second argument of the script (i.e. the buffer)
 
 
 # if a video path was not supplied, grab the reference
@@ -55,44 +57,46 @@ while True:
  
 
 	frame = imutils.resize(frame, width=600) # to make the video smaller and easier to analyse
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0) # to blur image and wipe out any high frequency noise
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # change video to HSV color space (used to choose the lower- and upperOrange)
  
 	
 	mask = cv2.inRange(hsv, orangeLower, orangeUpper) # it will create a mask for every object in that falls into the HSV range defined
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2) #dilate and erode to get rid of small defects in the mask
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] # cnts is the ensemble of countours that are in the mask (the objects with defined contours that are orange). RETR_EXTERNAL will outline only the external contour. CHAIN_APPROX_SIMPLE will draw the contour with the least number of points.
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2] # cnts is the ensemble of countours that are in the mask (the objects with defined contours that are orange). RETR_EXTERNAL will outline only the external contour. CHAIN_APPROX_SIMPLE will draw the contour with the least number of points.
  	center = None
 	
+
+
 	if len(cnts) > 0: # len(list) is the number of items in list. It will proceed only if at least one (orange object) was found.
 
-		for postits in cnts: #added this so that it will track several post-its at once
-			
-			pts = deque(maxlen=args["buffer"]) # pts will be a deque structure whose maximum length will be defined by the second argument of the script (i.e. the buffer)
-		
-		#	c = max(cnts, key=cv2.contourArea) # max will look in cnts and find the largest object as defined by their contour area. Had to take this out to track several at once.
-		
-			(x, y, w, h) = cv2.boundingRect(postits) # defines variables of postits as being a rectangle
-			M = cv2.moments(postits) 
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
- 
+		for c in cnts:
+
+			#c = max(cnts, key=cv2.contourArea) # max will look in cnts and find the largest object as defined by their contour area. Had to take this out to track several at once.
+	
+			(x, y, w, h) = cv2.boundingRect(c) # defines variables of c as being a square where the 3rd and 4th argument (w and h for a rectangle), are the same
+			M = cv2.moments(c) # moments is a measure that can be used to calculate several things, such as the centroid below.
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])) #the centroid of an object is defined like this
+	
+
 			# only proceed if the width meets a minimum size
 			if w > 10:
 				# draw the rectangle and centroid on the frame,
 				# then update the list of tracked points
 				cv2.rectangle(frame, (int(x), int(y)), (x+w,y+h), (0, 255, 0), 3) # 1st arg: image, 2nd: upper-left corner, 3rd: lower-right, 4th: color, 5th: thickness
 				cv2.rectangle(frame, center, center, (0, 255, 0), -1)
- 
-			pts.appendleft(center) # as center (centroid) moves, it will create a list by adding each new point. By default, the buffer is 32, so there will be 32 points in total. If I append to the right, the centroid will always have 1 as I move along and 31 will be added and then that point will dissapear. If I append to the left, the centroid will always have 32 points which will progressively dissapear in the tail. So for visualizing, it's better to append to the left, so we have a big point at the centroid, and the tail becomes thinner.
+ 	
+			shortestDistance = min(center, key=dist.euclidean)
 
+			pts.appendleft(shortestDistance) # as center (centroid) moves, it will create a list by adding each new point. By default, the buffer is 32, so there will be 32 points in total. If I append to the right, the centroid will always have 1 as I move along and 31 will be added and then that point will dissapear. If I append to the left, the centroid will always have 32 points which will progressively dissapear in the tail. So for visualizing, it's better to append to the left, so we have a big point at the centroid, and the tail becomes thinner.
+	
 				
 			for i in xrange(1, len(pts)): # loop over the set of tracked points
-	
+		
 				if pts[i - 1] is None or pts[i] is None: # if either of the tracked points are None, ignore them
 					continue
- 	
-
+ 		
+	
 				color = np.random.randint(0,255,3) # creates random integers. 1st: lowest number, 2nd: highest, 3rd: size of the array
 				thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 				cv2.line(frame, pts[i - 1], pts[i], color, thickness)
